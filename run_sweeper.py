30d72b8692ddc066ce6f382ffa062bafb58e82d3
Hongyan Xia <Jerryxia32@gmail.com>
2019-03-22 12:32:49 +0000

    Fix run script.

--- run_sweeper.py
+++ run_sweeper.py
@@ -3,13 +3,13 @@
 import sys
 import subprocess
 
-total_ms = 0
+bw = 0
 REPEAT = 20
 
 filenames = sys.argv[1:]
 for filename in filenames:
     for _ in range(REPEAT):
         result = subprocess.run(['./sweeper_elf', filename], stdout=subprocess.PIPE).stdout.decode()
-        total_ms += int(result.split()[2])
+        bw += float(result.split()[3])
 
-print(total_ms/len(filenames)/REPEAT)
+print(bw/len(filenames)/REPEAT)
